f3d337461729e1302201ca0f3a72633d83c6fce5
Inés María O <inesmao@misena.edu.co>
2022-11-08 22:14:51 -0500

    primer ejercicio de JS

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// este es un comentario de línea
+/*
+    Este es un comentario de bloque
+*/
+
+// Alt + Shift + A  Método abreviado comentario de bloque
+// Ctrl + } // Método abreviado comentario de línea
+
+alert("Hola Mundo, estoy aprendiendo JavaScript");
+alert("dos");
+alert("tres");
+alert("cuatro");
+alert("cinco");
+
+//console.log("Hola este es un mensaje poor consola")
+
+//TIPOS DE DATOS
+
+console.log(1234567)
+console.log("Inés")
+console.log(true)
+console.log(false)
+
+document.write("Hola estoy escribiendo en la Página web");
